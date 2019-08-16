2e75f0a15344b5da08f26f016fbd0113d8bd605d
Blake Smith <blakesmith@Blakes-MacBook-Pro.local>
2019-08-16 16:51:33 -0400

    update

--- assets/js/form.js
+++ assets/js/form.js
@@ -30,7 +30,7 @@ const sendMessage = function(ev) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
 
-    xhr.setRequestHeader('Content-Type', 'application/json');
+    //xhr.setRequestHeader('Content-Type', 'application/json');
 
 
     xhr.send(JSON.stringify({
